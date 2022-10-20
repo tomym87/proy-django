0e11e2bc4968762c08eade917fbbf029427fa574
Ubuntu <ubuntu@ip-172-31-40-235.us-east-2.compute.internal>
2022-10-20 17:14:24 +0000

    actualizando desde vps

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,14 +147,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ecommerce/static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT =BASE_DIR /'media'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
